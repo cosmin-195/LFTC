bf8130583d81211b35a846062a0e1ecc1f9470a0
Andrei Horatiu Mateescu <andreihoratiumateescu@yahoo.com>
2022-10-25 20:13:31 +0300

    fix pr problems

---
+++ lab3/main.py
@@ -0,0 +1,31 @@
+# 1 a
+
+class SymbolTable:
+
+    def __init__(self):
+        self.__capacity = 30
+        self.__table = [[] for _ in range(self.__capacity)]
+
+    def __hash(self, target):
+        if isinstance(target, str):
+            hash = 5381
+            for ch in target:
+                hash = ((hash << 5) + hash) + ord(ch)
+            return hash % self.__capacity
+        elif isinstance(target, int):
+            return target % self.__capacity
+
+    def add(self, value):
+        hash = self.__hash(value)
+        if value not in self.__table[hash]:
+            pos = len(self.__table[hash])
+            self.__table[hash].append(value)
+            return hash, pos
+        return hash, self.__table[hash].index(value)
+
+    def lookup(self, value):
+        key, val = value
+        if len(self.__table[key]) == 0 or len(self.__table[key]) - 1 < val:
+            return None
+        else:
+            return self.__table[key][val]
