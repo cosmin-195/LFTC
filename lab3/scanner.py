48e4cfd05c21a7ae8ce34437efaed3ca50dc8bdd
Cosmin <mcie2958@stud.ubbcluj.ro>
2022-11-02 22:19:18 +0200

    kj,,

--- lab3/scanner.py
+++ lab3/scanner.py
@@ -1,5 +1,6 @@
 # doc: regex, uml diagram, something else
 import re
+import symbolTable as sb
 
 
 class Scanner:
@@ -7,16 +8,38 @@ class Scanner:
     def __init__(self):
         self.operators = ['+', '-', '*', '/', '<', '<=', '=', '>=', 'eq', 'mod', 'not']
         self.keywords = ['init', 'verify', 'otherwise', 'escape', 'true', 'untrue', 'while', 'for', 'func', 'in']
-        self.separators = [']', '[', '{', '}', ';', ' ', '"', "'"]
-        self.pif = ()
-        self.st = ()
-        self.constSt = ()
+        self.separators = [']', '[', '{', '}', ';', ' ']
+        self.pif = []
+        self.st = sb.SymbolTable()
+        self.constSt = sb.SymbolTable()
 
     def __readFile(self):
         pass
 
     def scan(self, filename):
-        pass
+        with open('p1') as file:
+            line = file.readline()
+            line_nr = -1
+            while line:
+                line_nr += 1
+                p1 = 0
+                p2 = 0
+                while p2 < len(line):
+                    if line[p2] == ' ':
+                        p2 += 1
+                        p1 += 1
+                        continue
+                    if line[p2] in self.separators and line[p2] == '"':
+                        # todo instead call regex:
+                        #  if None ? exception : check start index to be same as p2 -> move p2 to end index
+                        match = match_string(line[p2:])
+                        if match is None:
+                            # todo write to file
+                            raise Exception('" is not matched - line' + str(line_nr))
+                        else:
+                            self.pif.append(("const", self.constSt.add(match.string)))
+
+                line = file.readline()
 
     def __classify(self, token):
         pass
@@ -26,26 +49,21 @@ def special_split(line):
     first = line.find('"')
 
 
-with open('p1') as file:
-    line = file.readline()
-    while line:
-        if '"' in line:
-            special_split(line)
-        else:
-            line.split()
-
-        line = file.readline()
-
-
 def match_int(s):
-    return re.match("-*[1-9]+[0-9]*",s)
+    return re.match("-*[1-9]+[0-9]*", s)
+
 
 def match_identifier(s):
-    return re.match("[a-zA-Z]+[a-zA-Z1-9_]*",s)
+    return re.match("[a-zA-Z]+[a-zA-Z1-9_]*", s)
+
+
+def match_string(s):
+    return re.match('"[^\n"]*"', s)
+
 
 st = ' fint = "vf d "'
-f =st.find('"')
+f = st.find('"')
+stb = sb.SymbolTable()
+stb.add(re.match('"[^\n"]*"','"fd da d " 3f').string)
+
 print(st.find('"',st.find('"',f+1)))
-
-
-
