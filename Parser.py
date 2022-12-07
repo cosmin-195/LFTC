6ef0cc7927222d250554915d5871a760b0043077
Cosmin Mateescu <mcie2958@scs.ubbcluj.ro>
2022-12-07 17:36:42 +0200

    cfg

--- Parser.py
+++ Parser.py
@@ -8,17 +8,30 @@ class Parser:
         self.grammar = grammar
 
     def closure(self, S):
-        result = set()
+        result = []
+        for item in S:
+            result.append(item)
         added_all_prod_of_nonterminal = {}
         for n in self.grammar.N:
             added_all_prod_of_nonterminal[n] = False
-        for item in S:
-            left, right = item.split("->")
-            if "." in right:
-                leftSideOfDot, rightSideOfDot = right.split(".".split())
-                nonterminal = rightSideOfDot[rightSideOfDot.find(".")+1]
-                if not added_all_prod_of_nonterminal[nonterminal]:
+        notDone = False
+        while (not notDone):
+            for item in result:
+                notDone = True
+                left, right = item.split("->")
+                if "." in right:
+                    leftSideOfDot, rightSideOfDot = right.strip().split(".")
+                    nonterminal = rightSideOfDot[rightSideOfDot.find(".") + 1]
+                    if nonterminal in self.grammar.N:
+                        if not added_all_prod_of_nonterminal[nonterminal]:
+                            added_all_prod_of_nonterminal[nonterminal] = True
+                            for production in self.grammar.P[nonterminal]:
+                                result.append(nonterminal + " -> ." + production)
+                                notDone = False
+        return result
 
 
-
-
+g = Grammar.Grammar()
+g.read_from_file("exempleCFG")
+p = Parser(g)
+print(p.closure(["Z->.S"]))
