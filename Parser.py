dabaf51518b67bbbf2c75949b4a6a2aaf2c7dd32
Cosmin <mcie2958@stud.ubbcluj.ro>
2022-12-14 14:38:53 +0200

    parsing

--- Parser.py
+++ Parser.py
@@ -16,22 +16,60 @@ class Parser:
             added_all_prod_of_nonterminal[n] = False
         notDone = False
         while (not notDone):
+            notDone = True
             for item in result:
-                notDone = True
                 left, right = item.split("->")
                 if "." in right:
                     leftSideOfDot, rightSideOfDot = right.strip().split(".")
-                    nonterminal = rightSideOfDot[rightSideOfDot.find(".") + 1]
-                    if nonterminal in self.grammar.N:
-                        if not added_all_prod_of_nonterminal[nonterminal]:
-                            added_all_prod_of_nonterminal[nonterminal] = True
-                            for production in self.grammar.P[nonterminal]:
-                                result.append(nonterminal + " -> ." + production)
-                                notDone = False
+                    rightSideOfDot = rightSideOfDot.strip()
+                    if len(rightSideOfDot) > 0:
+                        nonterminal = rightSideOfDot[0]
+                        if nonterminal in self.grammar.N:
+                            if not added_all_prod_of_nonterminal[nonterminal]:
+                                added_all_prod_of_nonterminal[nonterminal] = True
+                                for production in self.grammar.P[nonterminal]:
+                                    result.append(nonterminal + "->." + production)
+                                    notDone = False
         return result
+
+    def goto(self, S, x):
+        closureInput = []
+        for item in S:
+            left, right = item.split("->")
+            if "." in right:
+                leftSideOfDot, rightSideOfDot = right.strip().split(".")
+                rightSideOfDot = rightSideOfDot.strip()
+                if len(rightSideOfDot) == 0:
+                    continue
+                rightSymbol = rightSideOfDot[0]
+                if rightSymbol == x:
+                    newItem = left.strip() + "->" + leftSideOfDot.strip() + x + "." + rightSideOfDot[1:]
+                    closureInput.append(newItem)
+        return self.closure(closureInput) if len(closureInput) > 0 else []
+
+    def canonical_collection(self):
+        start_item = self.grammar.S + "_PRIME->." + self.grammar.S
+        c = []
+        c.append(self.closure([start_item]))
+        index = 0
+        modified = True
+        while modified:
+            modified = False
+            for item_set in c[index:]:
+                for symbol in self.grammar.N + self.grammar.E:
+                    result = self.goto(item_set, symbol)
+                    if len(result) > 0 and result not in c:
+                        c.append(result)
+                        modified = True
+        return c
 
 
 g = Grammar.Grammar()
-g.read_from_file("exempleCFG")
+# g.read_from_file("exampleCFG")
+g.read_from_file("grammar4.1")
 p = Parser(g)
-print(p.closure(["Z->.S"]))
+# print(p.closure(["E_PRIME->.E"]))
+for item in p.canonical_collection():
+    print(item)
+# print(p.closure(["Z->.S"]))
+# print(p.goto(["Z->.S"], "S"))
